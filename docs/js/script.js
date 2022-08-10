6af6dbb3ced1efb3580829d7f65a5bfe3b96fdaf
wasabi <shulgach2013@gmail.com>
2022-08-10 17:46:10 +0300

    second and third block

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,1 @@
-$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,autoplay:!0,autoplaySpeed:1e4,speed:600,infinite:!0,cssEase:"ease-in-out",touchThreshold:300})}));
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,autoplay:!1,autoplaySpeed:1e4,speed:600,infinite:!0,cssEase:"ease-in-out",touchThreshold:300})}));
--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.slider').slick({
         arrows: false,
         dots: true,
-        autoplay:true,
+        autoplay:false,
         autoplaySpeed: 10000,
         speed: 600,
         infinite: true,
